19788da37b2341048d287cca0fc23064235feec8
tby <1871666267@qq.com>
2020-04-15 14:58:34 +0800

    1

--- src/main/java/webadv/s99201105/p02/App.java
+++ src/main/java/webadv/s99201105/p02/App.java
@@ -3,7 +3,7 @@ package webadv.s99201105.p02;
 import org.apache.commons.codec.digest.DigestUtils;
 public class App {
     public static void main(String[] args) {
-		String name = "2342";
+		String name = "23142";
 		String password = sha256hex("117201319");
         if (args.length < 1) {
             System.err.println("Please provide an input!");
